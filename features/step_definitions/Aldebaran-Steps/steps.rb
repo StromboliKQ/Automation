4c1ece387efef3ce53103c304d427629bfd1522e
KonQuestNow <konquestnow@gmail.com>
2016-11-22 15:12:47 -0800

    Update steps.rb

--- features/step_definitions/Aldebaran-Steps/steps.rb
+++ features/step_definitions/Aldebaran-Steps/steps.rb
@@ -10,7 +10,7 @@ Given(/^I go to "([^"]*)" soundcloud page$/) do |user|
 end
 
 Given(/^I play a song$/) do
-  click_on '/html/body/div[1]/div[2]/div/div[4]/div[1]/div/div[2]/div/div[2]/ul/li[1]/div/div/div/div[2]/div[1]/div/div/div[1]/button'
+  first(:button, Play).click
 end
 
 Then(/^I wait$/) do
