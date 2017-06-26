9482da0da6a9828e43ac6dfffe0bfa9ef1035851
KonQuestNow <konquestnow@gmail.com>
2017-06-26 02:06:53 -0700

    Update steps.rb

--- features/step_definitions/Aldebaran-Steps/steps.rb
+++ features/step_definitions/Aldebaran-Steps/steps.rb
@@ -6,18 +6,15 @@
 
 #### Example Code: RUBY ####
 
-
 # => Navigate directly to the track
 Given(/^I play the track "([^"]*)"$/) do |url|
   chrome_capabilities = Selenium::WebDriver::Remote::Capabilities.chrome()
   firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox()
-
   chrome = Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => chrome_capabilities)
   firefox = Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => firefox_capabilities)
 
   chrome.get(url)
   puts chrome.title
-
   firefox.get(url)
   puts firefox.title
 end
@@ -35,8 +32,14 @@ end
 
 # => Wait for the song to play
 Then(/^I wait$/) do
+  chrome_capabilities = Selenium::WebDriver::Remote::Capabilities.chrome()
+  firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox()
+  chrome = Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => chrome_capabilities)
+  firefox = Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => firefox_capabilities)
+  
   wait = Selenium::WebDriver::Wait.new(:timeout => 40)
   #sleep(40)
+  
   chrome.quit
   firefox.quit
 end   
