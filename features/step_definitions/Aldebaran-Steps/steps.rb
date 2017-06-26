bed6a752e8c9d7702518b51f1fed14f4e9e62f0c
KonQuestNow <konquestnow@gmail.com>
2017-06-26 00:57:44 -0700

    Update steps.rb

--- features/step_definitions/Aldebaran-Steps/steps.rb
+++ features/step_definitions/Aldebaran-Steps/steps.rb
@@ -6,6 +6,13 @@
 
 #### Example Code: RUBY ####
 
+def setup
+chrome_capabilities = Selenium::WebDriver::Remote::Capabilities.chrome()
+firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox()
+chrome = Selenium::WebDriver.for(:remote, :url => 'http://127.0.0.1:4444/wd/hub', :desired_capabilities => chrome_capabilities)
+firefox = Selenium::WebDriver.for(:remote, :url => 'http://127.0.0.1:4444/wd/hub', :desired_capabilities => firefox_capabilities)
+end
+
 # => Navigate directly to the track
 Given(/^I play the track "([^"]*)"$/) do |url|
   chrome.get(url)
@@ -22,11 +29,11 @@ Given(/^I go to "([^"]*)" soundcloud page$/) do |user|
 end
 
 # => Click the Play button
-Given(/^I play a song$/) do
+Given(/^I share a song$/) do
   find('.sc-button.sc-button-xlarge', match: :first).click
 end
 
 # => Wait for the song to play
 Then(/^I wait$/) do
   sleep(40)
-end  
+end   
